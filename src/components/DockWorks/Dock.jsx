005ff2993f7afc364355ee467383c9ed3018dee6
ayushrayal19@gmail.com <ayushrayal19@gmail.com>
2026-04-03 22:30:07 +0530

    Portfolio Project Done.(Version 1.0)

--- src/components/DockWorks/Dock.jsx
+++ src/components/DockWorks/Dock.jsx
@@ -19,14 +19,18 @@ const Dock = ({ setWindowState }) => {
             ...state, resume:true,
           }))
         }}><img src="./Dock-icons/pdf.svg" alt="" /></div>
-        <div className="icon calender" ><img src="./Dock-icons/calender.svg" alt="" /></div>
         <div className="icon spotify" onClick={()=>{
           setWindowState(state=>({
             ...state, spotify:true,
           }))
         }}><img src="./Dock-icons/spotify.svg" alt="" /></div>
-        <div className="icon mail"><img src="./Dock-icons/mail.svg" alt="" /></div>
-        <div className="icon link"><img src="./Dock-icons/link.svg" alt="" /></div>
+        <div className="icon mail"
+        onClick={()=>{
+          window.open("mailto:ayushrayal19@gmail.com","_blank")
+        }}><img src="./Dock-icons/mail.svg" alt="" /></div>
+        <div className="icon link" onClick={()=>{
+          window.open("https://www.linkedin.com/in/ayush-rayal","_blank")
+        }}><img src="./Dock-icons/link.svg" alt="" /></div>
         <div className="icon cli" onClick={()=>{
           setWindowState(state=>({
             ...state, cli:true,
